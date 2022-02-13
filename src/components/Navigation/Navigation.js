9249e8d9697ed95c934e4a4a7ecff9f71ee8806f
Nwoye Ezekiel <chinecherem.nwoye.cn@gmail.com>
2022-02-13 18:00:03 +0100

    add mobile navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,13 +4,18 @@ import Logo from "./assets/logo.svg";
 import Hamburger from "./assets/hamburger.svg";
 import Tab from "./Tab/Tab";
 import { tabs } from "./tabs.js";
+import Arrow from "./Tab/assets/arrow-down.svg";
 
 export default function Navigation() {
   const [activeTab, setActiveTab] = useState(0);
   const [openNav, setOpenNav] = useState(false);
+  const [showDroplist, setShowDroplist] = useState(false);
 
   const handleNav = () => {
     setOpenNav(!openNav);
+  };
+  const handleDroplist = () => {
+    setShowDroplist(!showDroplist);
   };
 
   return (
@@ -24,18 +29,53 @@ export default function Navigation() {
         </div>
       </div>
       <div className="desktop-navigation">
-      <div className="tabs">
-        {tabs.map((tab, index) => (
-          <Tab
-            title={tab.title}
-            key={index}
-            active={index === activeTab}
-            onClick={() => setActiveTab(index)}
-            droplist={tab.droplist}
-          />
-        ))}
+        <div className="tabs">
+          {tabs.map((tab, index) => (
+            <Tab
+              title={tab.title}
+              key={index}
+              active={index === activeTab}
+              onClick={() => setActiveTab(index)}
+              droplist={tab.droplist}
+            />
+          ))}
+        </div>
       </div>
-      </div>
+
+      {openNav && (
+        <div className="mobile-links">
+          <div className="mobile-links-header">
+            <div className="logo">
+              <img src={Logo} alt="logo" />
+            </div>
+            <div onClick={handleNav} className="close-mobile-nav">
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+          {tabs.map((tab, index) => {
+            const droplist = tab.droplist;
+            return (
+              <div>
+                <li
+                  className="mobile-link"
+                  onClick={droplist ? handleDroplist : null}
+                  key={index}
+                >
+                  {tab.title} {droplist && <img src={Arrow} alt="" />}
+                </li>
+                {droplist &&
+                  showDroplist &&
+                  droplist.map((list, index) => (
+                    <li className="mobile-sub-link" key={index}>
+                      {list}
+                    </li>
+                  ))}
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Navigation/Tab/Tab.js
+++ src/components/Navigation/Tab/Tab.js
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./Tab.css";
+import Arrow from "./assets/arrow-down.svg";
 
-const Tab = ({ title, active = false, onClick, droplist }) => {
+const Tab = ({ title, active = false, onClick, droplist = null }) => {
+  const [showList, setShowList] = useState(false);
+  const wrapperRef = useRef(null);
+
+  const useOutsideAlerter = (ref) => {
+    useEffect(() => {
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setShowList(false);
+        }
+      }
+      document.addEventListener("mousedown", handleClickOutside);
+      return () =>
+        document.removeEventListener("mousedown", handleClickOutside);
+    }, [ref]);
+  };
+
+  const handleShowList = () => {
+    if (droplist) {
+      setShowList(!showList);
+    }
+  };
+
+  useOutsideAlerter(wrapperRef);
+
   return (
-    <div onClick={onClick} className={`tab ${active && "bold"}`}>
-      <span className={`${active ? "active" : "inactive"}`}>{title}</span>
+    <div onClick={onClick} className="tab">
+      <span
+        ref={wrapperRef}
+        onClick={handleShowList}
+        className={`${active && !droplist ? "active" : "inactive"}`}
+      >
+        {title}
+        {droplist && <img src={Arrow} alt="" />}
+        {droplist && showList && (
+          <div className="droplist-container">
+            {droplist.map((list) => (
+              <li>{list}</li>
+            ))}
+          </div>
+        )}
+      </span>
     </div>
   );
 };
